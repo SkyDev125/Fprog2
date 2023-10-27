7c93b883041f074d245a0c357957f1c04bf275e9
Diogo Santos (Sky) <diogo.goncalves.santos@tecnico.ulisboa.pt>
2023-10-27 17:46:08 +0100

    Added all goban structure functions

--- code/main.py
+++ code/main.py
@@ -278,18 +278,18 @@ def cria_goban(
         # Verify if there are no stones that share the same intersecao
         if inter in pretas:
             raise ValueError("cria_goban_vazio: argumento invalido")
-        go = coloca_pedra(inter)
+        go = coloca_pedra(go, inter, cria_pedra_branca())
 
     # Place the black pedras
     for inter in pretas:
-        go = coloca_pedra(inter)
+        go = coloca_pedra(go, inter, cria_pedra_preta())
 
     return go
 
 
 # Creates a copy of the goban
 def cria_copia_goban(go: goban) -> goban:
-    return go
+    return [[go[column][line] for line in range(len(go))] for column in range(len(go))]
 
 
 # Return the pedra in a given intersecao
@@ -305,7 +305,7 @@ def obtem_ultima_intersecao(go: goban) -> intersecao:
 
     Args:
     - go: A goban, where each element is a list representing a column of the territory.
-    Each cell of the column can be either 0, 1 or 2.
+    Each cell of the column can be either a black, white, or neutral stone.
 
     Returns:
     - A intersecao with a string and an integer representing the coordinates of the last intersection (top right) of the goban.
@@ -320,7 +320,7 @@ def obtem_cadeia(go: goban, inter: intersecao) -> tuple[intersecao]:
 
     Args:
     - go: a goban, where each element is a intersecao representing a column of the goban.
-    Each cell of the column can be either 0 or 1.
+    Each cell of the column can be either a black, white, or neutral stone.
     - inter: A intersecao containing a string and an integer
 
     Returns:
@@ -339,7 +339,7 @@ def obtem_cadeia(go: goban, inter: intersecao) -> tuple[intersecao]:
         chain = (inter,)
         visited += chain
 
-        # Check if the adjacent intersections are equal to the freedom
+        # Check if the adjacent intersections are equal to the pedra
         for inter in obtem_intersecoes_adjacentes(inter, obtem_ultima_intersecao(go)):
             if inter not in visited and obtem_pedra(go, inter) == is_free:
                 chain += recursive_check(go, inter, visited)
@@ -380,7 +380,7 @@ def eh_goban(go: any) -> bool:
 
     Args:
     - go: A goban, where each element is a column of the goban.
-    Each cell of the column can be either 0, 1 or 2.
+    Each cell of the column can be either a black, white, or neutral stone.
 
     Returns:
     - True if the goban is valid, False otherwise.
@@ -400,7 +400,7 @@ def eh_goban(go: any) -> bool:
         if not isinstance(column, list):
             return False
 
-        # Check if the column is empty or larger than expected
+        # Check if the column is smaller or larger than expected
         if len(column) not in (9, 13, 19):
             return False
 
@@ -414,7 +414,11 @@ def eh_goban(go: any) -> bool:
             if type(cell) != int:
                 return False
 
-            if cell not in (cria_pedra_neutra(), cria_pedra_branca(), cria_pedra_preta()):
+            if cell not in (
+                cria_pedra_neutra(),
+                cria_pedra_branca(),
+                cria_pedra_preta(),
+            ):
                 return False
 
     return True
@@ -427,7 +431,7 @@ def eh_intersecao_valida(go: goban, inter: intersecao) -> bool:
 
     Args:
     - go: a goban, where each element is a list representing a column of the goban.
-    Each cell of the column can be either 0, 1 or 2.
+    Each cell of the column can be either a black, white, or neutral stone.
     - inter: A intersecao containing a string and an integer.
 
     Returns:
@@ -459,7 +463,7 @@ def goban_para_str(go: goban) -> str:
 
     Args:
     - go: a goban, where each element is a list representing a column of the goban.
-    Each cell of the column can be a pedra(black or white) or not.
+    Each cell of the column can be either a black, white, or neutral stone.
 
     Returns:
     - A string representation of the goban, formatted as a grid with letters representing columns and numbers
@@ -497,35 +501,108 @@ def goban_para_str(go: goban) -> str:
     # Join the string
     return "".join(s)
 
-# Return a tuple of intercections that are empty
+
+# Return a tuple of intersections that are empty
 def obtem_territorios(go: goban) -> tuple[tuple[intersecao]]:
     """
     Returns the free chains in a goban (territories).
 
     Args:
     - go: A goban, where each element is a line representing a column of the territory.
-    Each cell of the column can be either 0 or 1.
+    Each cell of the column can be either a black, white, or neutral stone.
 
     Returns:
     - A tuple of tuples representing free chains in the goban, sorted by their position from left to right
     and bottom to top.
     """
-    # Create a list of intersections
-    chain = ()   
+    all_chains = get_chains(go)
+    chains = ()
 
-    size = len(go)
-    # Check all the intersections
-    for column in range(size):
-        for cell in range(size):
-            # Convert to intercesao (0 -> A) (0 -> 1)
-            inter = cria_intersecao(chr(ord('A') + cell), 1 + column)
-            
-            if inter not in chain:
-                # Check if the intercesao is free
-                if obtem_pedra(go, inter) == cria_pedra_neutra():
-                    chain += (obtem_cadeia(go, inter),) 
+    # save all chains that are empty
+    for chain in all_chains:
+        if obtem_pedra(go, chain[0]) == cria_pedra_neutra():
+            chains += (chain,)
 
-    return ordena_intersecoes(chain)
+    return chains
+
+
+# Return a tuple of different adjacent intersections
+def obtem_adjacentes_diferentes(
+    go: goban, chain: tuple[intersecao]
+) -> tuple[intersecao]:
+    new_chain = ()
+
+    # Check each intersection in chain and return only the oposite adjacent intersections. (if player -> non player)
+    for inter in chain:
+        if eh_pedra_jogador(obtem_pedra(go, inter)):
+            for intr in obtem_intersecoes_adjacentes(
+                inter, obtem_ultima_intersecao(go)
+            ):
+                if (intr not in new_chain) and not eh_pedra_jogador(
+                    obtem_pedra(go, intr)
+                ):
+                    new_chain += (intr,)
+        else:
+            for intr in obtem_intersecoes_adjacentes(
+                inter, obtem_ultima_intersecao(go)
+            ):
+                if (intr not in new_chain) and eh_pedra_jogador(obtem_pedra(go, intr)):
+                    new_chain += (intr,)
+
+    return ordena_intersecoes(new_chain)
+
+
+# Return a new goban after a determined play
+def jogada(go: goban, inter: intersecao, p: pedra):
+    go = coloca_pedra(go, inter, p)
+    intrs = obtem_intersecoes_adjacentes(inter, obtem_ultima_intersecao(go))
+    chains = ()
+
+    # Get all the chains that are of the different player
+    for intr in intrs:
+        if (
+            eh_pedra_jogador(obtem_pedra(go, intr))
+            and (p != obtem_pedra(go, intr))
+            and (intr not in chains)
+        ):
+            chains += (obtem_cadeia(go, intr),)
+
+    # Substitute the trapped chain with empty intersections
+    for chain in chains:
+        adjacentes = obtem_adjacentes_diferentes(go, chain)
+        # Verify if there are any empty adjacent intersections
+        if len(adjacentes) == 0:
+            for intr in chain:
+                go = coloca_pedra(go, intr, cria_pedra_neutra())
+    return go
+
+
+# Return the ammount of white and black stones
+def obtem_pedras_jogadores(go: goban) -> tuple[int, int]:
+    """
+    Returns the ammount of white and black stones in a goban.
+
+    Args:
+    - go: A goban, where each element is a line representing a column of the territory.
+    Each cell of the column can be either a black, white, or neutral stone.
+
+    Returns:
+    - A tuple of integers representing the black and white stones in the territory respectively (white, black)
+    and bottom to top.
+    """
+    # Create the lists of occupied intersections
+    all_chains = get_chains(go)
+    white_chains = ()
+    black_chains = ()
+
+    # save all chains that are empty
+    for chain in all_chains:
+        if obtem_pedra(go, chain[0]) == cria_pedra_branca():
+            white_chains += chain
+        elif obtem_pedra(go, chain[0]) == cria_pedra_preta():
+            black_chains += chain
+
+    return len(white_chains), len(black_chains)
 
 
 """
@@ -547,3 +624,35 @@ def convert_intersecao(inter: intersecao) -> tuple[int, int]:
     """
     collumn, line = obtem_col(inter), obtem_lin(inter)
     return (ord(collumn) - (ord("A") - 1) - 1, line - 1)
+
+
+# Return a tuple of all intersection
+def get_chains(go: goban) -> tuple[tuple[intersecao]]:
+    """
+    Returns the all chains in a goban.
+
+    Args:
+    - go: A goban, where each element is a line representing a column of the territory.
+    Each cell of the column can be either a black, white, or neutral stone.
+
+    Returns:
+    - A tuple of tuples representing all the chains in the goban, sorted by their position from left to right
+    and bottom to top.
+    """
+    # Create a list of intersections
+    chain = ()
+    visited = ()
+
+    size = len(go)
+    # Check all the intersections
+    for cell in range(size):
+        for collumn in range(size):
+            # Convert to intercesao (0 -> A) (0 -> 1)
+            inter = cria_intersecao(chr(ord("A") + collumn), 1 + cell)
+
+            if inter not in visited:
+                temp = obtem_cadeia(go, inter)
+                visited += temp
+                chain += (temp,)
+
+    return chain
